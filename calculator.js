26dd3142e82e15c16c4e908f5114315983bae5c0
Clam119 <Christopher.lam119@gmail.com>
2022-04-03 14:50:47 +0100

    initial commit

---
+++ calculator.js
@@ -0,0 +1,41 @@
+// Top Section - Importing the Express Framework, bodyParser package (Used to parse info from requests so that it's usable)
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+const port = 3000
+
+
+//Root GET Requests
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+})
+
+//Root POST Requests
+app.post('/', function (req, res) {
+    let firstUserNumber = Number(req.body.firstUserNumber);
+    let secondUserNumber = Number(req.body.secondUserNumber);
+    let result = Number(firstUserNumber + secondUserNumber);
+    res.send("The result of the calculation is " + result);
+});
+
+//BMI Calculator GET Requests
+app.get('/bmiCalculator', function (req, res) {
+    res.sendFile(__dirname + '/bmiCalculator.html');
+})
+
+//BMI Calculator POST Requests
+app.post('/bmiCalculator', function (req, res) {
+    let height = Number(req.body.height);
+    let weight = Number(req.body.weight);
+    let heightSquared = Math.pow(height, 2);
+    let bmiResult = weight / heightSquared;
+    res.send("Your BMI is " + bmiResult.toFixed(1));
+})
+
+//Listening to Ports
+app.listen(port, function () {
+    console.log('Calculator Application listening on port ${port}')
+})
